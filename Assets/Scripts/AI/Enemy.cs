3bde9afe02a5397d4907e65b05279aa114375ce7
Connor SAE <connor.westhuizen@student.saedubai.com>
2025-02-18 17:07:05 +0400

    Enemy restructure

--- Assets/Scripts/AI/Enemy.cs
+++ Assets/Scripts/AI/Enemy.cs
@@ -7,8 +7,6 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotateSpeed;
     [SerializeField] private float targetDistanceThreshold;
-    [SerializeField] private int maxHealth = 10;
-    private int health;
 
     protected Vector3 targetPos;
 
@@ -21,44 +19,10 @@ public abstract class Enemy : MonoBehaviour
             GoToTarget();
         }
     }
-
-    protected virtual void Start()
-    {
-        health = maxHealth;
-    }
     #endregion
 
     public abstract void Attack();
 
-    /// <summary>
-    /// Decreases Enemy health by the given amount
-    /// </summary>
-    /// <param name="amount"></param>
-    public void TakeDamage(int amount)
-    {
-        health -= Mathf.Min(amount, 0); // damage cannot be posotive
-
-        if(health <= 0)
-        {
-            Die();
-        }
-    }
-
-    public void Heal(int amount)
-    {
-        health += Mathf.Max(amount, 0); // damage cannot be negative
-
-        if (health > maxHealth)
-        {
-            health = maxHealth;
-        }
-    }
-
-    protected void Die()
-    {
-        Debug.Log(gameObject.name + " was Killed");
-        Destroy(gameObject);
-    }
 
     private void GoToTarget()
     {
