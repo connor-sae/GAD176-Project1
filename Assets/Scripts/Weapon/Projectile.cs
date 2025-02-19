74184fb6edc879fdc01d927cdc64cdca3b9ac131
ConnorSAE <connor.westhuizen@student.saedubai>
2025-02-19 11:48:16 +0400

    requires entity

---
+++ Assets/Scripts/Weapon/Projectile.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class Projectile : MonoBehaviour
+{
+    /// <summary>
+    /// ignore all objects in these layers
+    /// </summary>
+    [SerializeField] LayerMask ignoreLayers;
+    /// <summary>
+    /// ignore's all objects in list, less performant than ignoreLayers
+    /// </summary>
+    [SerializeField] GameObject[] ignoreObjects;
+    [SerializeField] float speed;
+    [SerializeField] private int damage;
+
+    #region UnityFunctions
+
+    void Awake()
+    {
+        GetComponent<Collider>().excludeLayers = ignoreLayers;
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+    }
+
+    #endregion
+
+    public void OverrideDamage(int value)
+    {
+        damage = value;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach(GameObject ignoreObject in ignoreObjects)
+        {
+            if(collision.collider.gameObject == ignoreObject)
+            return;
+        }
+    }
+
+}
